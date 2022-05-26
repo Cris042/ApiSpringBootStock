694e4dc05cf5800a1ed0e6c64212505fd2a9cfad
Cristóvão Augusto <46114578+jovenlegolas@users.noreply.github.com>
2022-05-26 10:31:15 -0300

    Swagger

--- src/test/java/com/api/parkingcontrol/controller/ParkingControllerTests.java
+++ src/test/java/com/api/parkingcontrol/controller/ParkingControllerTests.java
@@ -1,21 +1,16 @@
 package com.api.parkingcontrol.controller;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
-import io.restassured.http.ContentType;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+
 
 import com.api.parkingcontrol.modules.parking.controllers.ParkingSpotController;
 import com.api.parkingcontrol.modules.parking.services.ParkingSpotService;
-import com.api.parkingcontrol.modules.user.controllers.UserController;
-import com.api.parkingcontrol.modules.user.services.UserService;
 
 @WebMvcTest
 public class ParkingControllerTests {
@@ -37,12 +32,12 @@ public class ParkingControllerTests {
 
 		// // when(this.parkingSpotService.findAll(null)).thenReturn( null);
 		
-		given()
-			.accept(ContentType.JSON)
-		.when()
-			.get("/parking-spot/")
-		.then()
-			.statusCode(HttpStatus.OK.value());
+		// given()
+		// 	.accept(ContentType.JSON)
+		// .when()
+		// 	.get("/parking-spot/")
+		// .then()
+		// 	.statusCode(HttpStatus.OK.value());
 	}
 
     // @Test
