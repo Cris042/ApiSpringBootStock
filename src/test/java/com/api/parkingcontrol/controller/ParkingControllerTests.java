67a8a49ae28b5dff9d437cf1a93f0d3fcb889906
Cristóvão Augusto <46114578+jovenlegolas@users.noreply.github.com>
2022-05-28 22:44:58 -0300

    Configurando Mockito

--- src/test/java/com/api/parkingcontrol/controller/ParkingControllerTests.java
+++ src/test/java/com/api/parkingcontrol/controller/ParkingControllerTests.java
@@ -1,58 +1,38 @@
 package com.api.parkingcontrol.controller;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-
 import com.api.parkingcontrol.modules.parking.controllers.ParkingSpotController;
+import com.api.parkingcontrol.modules.parking.entities.ParkingSpotEntities;
 import com.api.parkingcontrol.modules.parking.services.ParkingSpotService;
 
-@WebMvcTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ParkingControllerTests.class)
 public class ParkingControllerTests {
 
-    @Autowired
-	private ParkingSpotController parkingSpotController;
+	@InjectMocks
+	ParkingSpotController parkingSpotController;
 
-	@MockBean
-	private ParkingSpotService parkingSpotService;
+	@Mock
+	ParkingSpotService parkingSpotService;
 
-    @BeforeEach
-	public void setup() 
-    {
-		standaloneSetup( this.parkingSpotController );
+	@Test
+	public void testBasicScenario() 
+	{
+		Page<ParkingSpotEntities> pages = parkingSpotService.findAll(null); 
+		Mockito.when( parkingSpotController.getAllParkingSpots( (Pageable) pages ))
+		.thenReturn( null );
 	}
 
-    @Test
-	public void listAllParkingSpots() {
-
-		// // when(this.parkingSpotService.findAll(null)).thenReturn( null);
-		
-		// given()
-		// 	.accept(ContentType.JSON)
-		// .when()
-		// 	.get("/parking-spot/")
-		// .then()
-		// 	.statusCode(HttpStatus.OK.value());
-	}
-
-    // @Test
-	// public void listParkingSpots() {
-
-    //     UUID id = UUID.randomUUID();
-
-	// 	when(this.parkingSpotService.findById( id )).thenReturn(null);
-	// 	given()
-	// 		.accept(ContentType.JSON)
-	// 	.when()
-	// 		.get("/parking-spot/{id}", id)
-	// 	.then()
-	// 		.statusCode(HttpStatus.OK.value());
-	// }
-	
+    
 	
 }
