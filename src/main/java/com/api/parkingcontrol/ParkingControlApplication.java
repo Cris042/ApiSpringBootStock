f3dab5f30ac74923b898c137f48f9b031bed6bea
Cristóvão Augusto <46114578+jovenlegolas@users.noreply.github.com>
2022-05-29 16:18:23 -0300

    Chamar api externa

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +15,12 @@ public class ParkingControlApplication {
 	}
 
 	@GetMapping("/")
-	public String index(){
-		return "Olá Mundo!";
-	}
+	public Object[] getCep() {
+
+		String url = "https://restcountries.com/v2/all";
+		RestTemplate restTemplate = new RestTemplate();
+
+		Object[] result = restTemplate.getForObject(url, Object[].class);
+		return result;
+    }
 }
